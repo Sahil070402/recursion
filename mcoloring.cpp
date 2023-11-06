077a0dc06793bb6af5bf426f2fbe4621a64a121e
Sahil070402 <sahilsareen0007@gmail.com>
2023-11-07 00:36:57 +0530

    day 3

---
+++ mcoloring.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
+    bool placable(int node, int j, bool graph[101][101], int n, vector<int> &color)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            if (graph[node][k] == 1 && color[k] == j)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool helper(bool graph[101][101], int m, int n, int node, vector<int> &color)
+    {
+        if (node == n)
+        {
+            return true;
+        }
+        for (int j = 0; j < m; j++)
+        {
+            if (placable(node, j, graph, n, color))
+            {
+                color[node] = j;
+                if (helper(graph, m, n, node + 1, color) == true)
+                {
+                    return true;
+                }
+                color[node] = -1;
+            }
+        }
+        return false;
+    }
+    bool graphColoring(bool graph[101][101], int m, int n)
+    {
+        vector<int> color(n, -1);
+        return helper(graph, m, n, 0, color);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, e;
+        cin >> n >> m >> e;
+        int i;
+        bool graph[101][101];
+        for (i = 0; i < n; i++)
+        {
+            memset(graph[i], 0, sizeof(graph[i]));
+        }
+        for (i = 0; i < e; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            graph[a - 1][b - 1] = 1;
+            graph[b - 1][a - 1] = 1;
+        }
+        Solution ob;
+        cout << ob.graphColoring(graph, m, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
